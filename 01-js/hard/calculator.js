b0ee912dadf8a7d88534da4cf599a9eae3ad22cb
Subhav Mittal <sm16@Subhavs-MacBook-Air.local>
2026-02-11 02:36:56 +0530

    done js-builtins and 01-js

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -17,6 +17,130 @@
   - `npm run test-calculator`
 */
 
-class Calculator { }
+class Calculator { 
+  constructor() {
+    this.result = 0;
+  }
+  add(num){
+    this.result += num
+  }
+  subtract(num){
+    this.result-=num
+  }
+  multiply(num){
+    this.result*=num
+  }
+  divide(num){
+    if(num === 0){
+      throw new Error("Cannot divide by zero")
+    }
+    this.result/=num
+  }
+  clear(){
+    this.result=0
+  }
+  getResult(){
+    return this.result
+  }
+  calculate(calcStr){
+    // Remove all spaces
+    const expr = calcStr.replace(/\s+/g, '');
+
+    // Validate expression - should only contain numbers, operators, parentheses, and decimal points
+    if (!/^[0-9+\-*/.()]+$/.test(expr)) {
+      throw new Error("Invalid Expression");
+    }
+
+    let pos = 0;
+
+    const parseExpression = () => {
+      let result = parseTerm();
+
+      while (pos < expr.length && (expr[pos] === '+' || expr[pos] === '-')) {
+        const op = expr[pos++];
+        const term = parseTerm();
+        if (op === '+') {
+          result += term;
+        } else {
+          result -= term;
+        }
+      }
+
+      return result;
+    };
+
+    const parseTerm = () => {
+      let result = parseFactor();
+
+      while (pos < expr.length && (expr[pos] === '*' || expr[pos] === '/')) {
+        const op = expr[pos++];
+        const factor = parseFactor();
+        if (op === '*') {
+          result *= factor;
+        } else {
+          if (factor === 0) {
+            throw new Error("Division by zero");
+          }
+          result /= factor;
+        }
+      }
+
+      return result;
+    };
+
+    const parseFactor = () => {
+      // Handle negative numbers
+      if (expr[pos] === '-') {
+        pos++;
+        return -parseFactor();
+      }
+
+      // Handle parentheses
+      if (expr[pos] === '(') {
+        pos++;
+        const result = parseExpression();
+        if (expr[pos] !== ')') {
+          throw new Error("Invalid Expression");
+        }
+        pos++;
+        return result;
+      }
+
+      // Parse number
+      let numStr = '';
+      while (pos < expr.length && (expr[pos] >= '0' && expr[pos] <= '9' || expr[pos] === '.')) {
+        numStr += expr[pos++];
+      }
+
+      if (numStr === '') {
+        throw new Error("Invalid Expression");
+      }
+
+      return parseFloat(numStr);
+    };
+
+    try {
+      this.result = parseExpression();
+
+      // Check for unmatched parentheses or extra characters
+      if (pos !== expr.length) {
+        throw new Error("Invalid Expression");
+      }
+
+      if (!isFinite(this.result)) {
+        throw new Error("Division by zero");
+      }
+    } catch (error) {
+      throw new Error("Invalid Expression");
+    }
+
+    return this.result;
+  }
+
+}
+
+calc = new Calculator();
+calc.calculate('2+3*4')
+console.log(calc.getResult())
 
 module.exports = Calculator;
--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,7 +12,51 @@
 */
 
 class Todo {
-
+  constructor() {
+    this.todos = [];
+    this.length = 0;
+  }
+  add(todo) {
+    this.todos.push(todo);
+    this.length++;
+  }
+  remove(indexOfTodo) {
+    this.todos.splice(indexOfTodo, 1);
+    this.length--;
+  }
+  update(index, updatedTodo) {
+    if (index > (this.length - 1)) {
+      return;
+    }
+    this.todos[index] = updatedTodo;
+  }
+  getAll() {
+    return this.todos;
+  }
+  get(index) {
+    if (index > (this.length - 1)) {
+      return null;
+    }
+    return this.todos[index];
+  }
+  clear() {
+    this.todos = [];
+  }
 }
 
+const todoList = new Todo();
+todoList.add("Task 1");
+todoList.add("Task 2");
+todoList.add("Task 3");
+console.log(todoList.getAll());
+todoList.remove(1);
+console.log(todoList.getAll());
+todoList.remove(0);
+console.log(todoList.getAll());
+todoList.remove(2);
+console.log(todoList.getAll());
+todoList.update(1, 'Updated Task 2');
+console.log(todoList.get(1));
+todoList.update(3, 'Invalid Task')
+console.log(todoList.getAll());
 module.exports = Todo;
